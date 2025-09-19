dee45056f51f6ab562b551df0204c4775786bd3d
pq4u <paweltes19@gmail.com>
2025-09-19 14:47:02 +0200

    Create get meetings handler

---
+++ src/Apex.Application/Queries/Meetings/GetMeetingsQuery.cs
@@ -0,0 +1,7 @@
+﻿using Apex.Application.Abstractions;
+using Apex.Application.DTO;
+using Apex.Domain.Entities;
+
+namespace Apex.Application.Queries.Meetings;
+
+public record GetMeetingsQuery() : IQuery<List<Meeting>?>;
---
+++ src/Apex.Application/Queries/Meetings/GetMeetingsQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using Apex.Application.Abstractions;
+using Apex.Application.DTO;
+using Apex.Domain.Entities;
+using Apex.Domain.Repositories;
+
+namespace Apex.Application.Queries.Meetings;
+
+public class GetMeetingsQueryHandler : IQueryHandler<GetMeetingsQuery, List<Meeting>?>
+{
+    private readonly IMeetingRepository _meetingRepository;
+
+    public GetMeetingsQueryHandler(IMeetingRepository meetingRepository)
+        => _meetingRepository = meetingRepository;
+
+    public async Task<List<Meeting>?> HandleAsync(GetMeetingsQuery query, CancellationToken cancellationToken = default)
+        => await _meetingRepository.GetAllAsync(cancellationToken);
+}
