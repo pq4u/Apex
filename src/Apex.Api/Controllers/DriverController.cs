5a86282876d52175952864b8fc5d7cc20477251d
pq4u <paweltes19@gmail.com>
2025-09-23 20:46:32 +0200

    Add driver controller

---
+++ src/Apex.Api/Controllers/DriverController.cs
@@ -0,0 +1,38 @@
+﻿using Apex.Application.Abstractions;
+using Apex.Application.DTO.Api;
+using Apex.Application.Queries.Drivers;
+using Apex.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apex.Api.Controllers;
+
+[ApiController]
+[Route("drivers")]
+public class DriverController : ControllerBase
+{
+    private readonly IQueryHandler<GetSessionDriversQuery, IEnumerable<Driver>> _getDriverLapsInSessionQueryHandler;
+
+    public DriverController(IQueryHandler<GetSessionDriversQuery, IEnumerable<Driver>> getDriverLapsInSessionQueryHandler)
+    {
+        _getDriverLapsInSessionQueryHandler = getDriverLapsInSessionQueryHandler;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<DriverResultDto>>> GetDriversInSession([FromQuery] GetSessionDriversQuery command)
+    {
+        var driversInSession = await _getDriverLapsInSessionQueryHandler.HandleAsync(command);
+
+        var result = driversInSession.Select(x => new DriverResultDto
+        {
+            Id = x.Id,
+            FirstName = x.FirstName,
+            LastName = x.LastName,
+            NameAcronym = x.NameAcronym,
+            DriverNumber = x.DriverNumber,
+            CountryCode = x.CountryCode,
+            HeadshotUrl = x.HeadshotUrl
+        });
+
+        return Ok(result);
+    }
+}
---
+++ src/Apex.Application/DTO/Api/DriverResultDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Apex.Application.DTO.Api;
+
+public class DriverResultDto
+{
+    public int Id { get; set; }
+    public int DriverNumber { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string NameAcronym { get; set; } = null!;
+    public string? HeadshotUrl { get; set; }
+    public string CountryCode { get; set; } = null!;
+}
