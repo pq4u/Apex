7b979f0baf63497d66ec46b700837fd0265e9ef6
pq4u <paweltes19@gmail.com>
2025-09-23 18:29:02 +0200

    Add session controller

---
+++ src/Apex.Api/Controllers/SessionController.cs
@@ -0,0 +1,37 @@
+﻿using Apex.Application.Abstractions;
+using Apex.Application.DTO.Api;
+using Apex.Application.Queries.Sessions;
+using Apex.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apex.Api.Controllers;
+
+[ApiController]
+[Route("sessions")]
+public class SessionController : ControllerBase
+{
+    private readonly IQueryHandler<GetSessionsQuery, IEnumerable<Session>> _getMeetingsQueryHandler;
+
+    public SessionController(IQueryHandler<GetSessionsQuery, IEnumerable<Session>> getMeetingsQueryHandler)
+    {
+        _getMeetingsQueryHandler = getMeetingsQueryHandler;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<SessionResultDto>>> Get()
+    {
+        var query = new GetSessionsQuery();
+        var sessions = await _getMeetingsQueryHandler.HandleAsync(query);
+
+        var result = sessions.Select(x => new SessionResultDto
+        {
+            Id = x.Id,
+            MeetingId = x.MeetingId,
+            Name = x.Name,
+            StartDate = x.StartDate,
+            EndDate = x.EndDate
+        });
+        
+        return Ok(result);
+    }
+}
---
+++ src/Apex.Application/DTO/Api/SessionResultDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Apex.Application.DTO.Api;
+
+public class SessionResultDto
+{
+    public int Id { get; set; }
+    public int MeetingId { get; set; }
+    public string Name { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
