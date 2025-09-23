745724306c5219f7dd16f96fed9b46009b77b50d
pq4u <paweltes19@gmail.com>
2025-09-23 21:10:55 +0200

    Add telemetry controller

---
+++ src/Apex.Api/Controllers/TelemetryController.cs
@@ -0,0 +1,41 @@
+﻿using Apex.Application.Abstractions;
+using Apex.Application.DTO.Api;
+using Apex.Application.Queries.Telemetry;
+using Apex.Domain.TimeSeries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apex.Api.Controllers;
+
+[ApiController]
+[Route("telemetry")]
+public class TelemetryController : ControllerBase
+{
+    private readonly IQueryHandler<GetTelemetryQuery, IEnumerable<TelemetryData>> _getTelemetryQueryHandler;
+
+    public TelemetryController(IQueryHandler<GetTelemetryQuery, IEnumerable<TelemetryData>> getTelemetryQueryHandler)
+    {
+        _getTelemetryQueryHandler = getTelemetryQueryHandler;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TelemetryDataResultDto>>> Get([FromQuery] GetTelemetryQuery query)
+    {
+        var telemetryData = await _getTelemetryQueryHandler.HandleAsync(query);
+
+        var result = telemetryData.Select(x => new TelemetryDataResultDto
+        {
+            Time = x.Time,
+            SessionId = x.SessionId,
+            DriverId = x.DriverId,
+            Speed = x.Speed,
+            Rpm = x.Rpm,
+            Gear = x.Gear,
+            Throttle = x.Throttle,
+            Brake = x.Brake,
+            Drs = x.Drs,
+            NGear = x.NGear,
+        });
+
+        return Ok(result);
+    }
+}
---
+++ src/Apex.Application/DTO/Api/TelemetryDataResultDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Apex.Application.DTO.Api;
+
+public class TelemetryDataResultDto
+{
+    public DateTime Time { get; set; }
+    public int SessionId { get; set; }
+    public int DriverId { get; set; }
+    public short Speed { get; set; }
+    public short Rpm { get; set; }
+    public short Gear { get; set; }
+    public short Throttle { get; set; }
+    public short Brake { get; set; }
+    public short Drs { get; set; }
+    public short NGear { get; set; }
+}
