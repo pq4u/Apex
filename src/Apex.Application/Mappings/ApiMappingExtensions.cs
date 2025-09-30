58f81521e96defacdf44dedd08aabbacabf2f76a
pq4u <paweltes19@gmail.com>
2025-09-30 21:13:20 +0200

    Add api mapping extensions

---
+++ src/Apex.Application/Mappings/ApiMappingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Apex.Application.DTO.Api;
+using Apex.Domain.Entities;
+
+namespace Apex.Application.Mappings;
+
+public static class ApiMappingExtensions
+{
+    public static DriverResultDto ToEntity(this Driver driver) => new DriverResultDto
+    {
+        Id = driver.Id,
+        FirstName = driver.FirstName,
+        LastName = driver.LastName,
+        NameAcronym = driver.NameAcronym,
+        DriverNumber = driver.DriverNumber,
+        CountryCode = driver.CountryCode,
+        HeadshotUrl = driver.HeadshotUrl
+    };
+}
